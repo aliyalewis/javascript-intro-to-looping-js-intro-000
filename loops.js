4c2b288c9e685692d67f4ca13e26c16f74c26c76
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-17 15:19:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,33 +1,30 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-  array.push("I am 1 strange loop.")}
-    else {
-  array.push(`I am ${i} strange loops.`)
+  for (let i=0; i<25; i++) {
+   if (i === 1) {
+    array.push("I am 1 strange loop.")
+  }else {
+    array.push(`I am ${i} strange loops.`)
   }
-  }
+}
   return array
 }
 
 function whileLoop(n) {
   let countdown = n;
-  while (countdown >0) {
-    console.log(--countdown);
+  while(countdown >0) {
+    console.log(--countdown)
   }
-  return 'done'
+  return "done"
 }
 
-
-
-
-
-function doWhileLoop(int)  {
-  var num = 0
-  function incrementVariable() {
-        num = num + 1;
-      return num;
-  }
-  do {
+function doWhileLoop(num) {
+var i = 0
+function incrementVariable() {
+  i = i+1;
+  return i;
+}
+do {
   console.log("I run once regardless.");
-} while (incrementVariable() < int)
+} while(incrementVariable() < num);
+return num;
 }
